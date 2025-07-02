31d1cd9e16d2c1b97aafc5c2daa3d03338fffce1
bonnyndush99 <bonnyndush99@gmail.com>
2025-07-01 23:49:58 -0400

    initial commit

---
+++ Calculator operations.py
@@ -0,0 +1,7 @@
+number = int(input("Enter an Integer number: "))
+remainder = number % 2
+if (remainder == 0):
+    print(number," is an even number")
+else:
+    (print(number," is an odd number"))
+
