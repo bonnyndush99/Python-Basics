5cfd981ca2d62c77dbc611992fe311d716628c74
bonnyndush99 <bonnyndush99@gmail.com>
2025-07-02 00:28:36 -0400

    initial commit

---
+++ Finding the Largest number.py
@@ -0,0 +1,12 @@
+n1 = int(input("Enter the first number : "))
+n2 = int(input("Enter the second number : "))
+n3 = int(input("Enter the third number : "))
+if      n1>n2 and n1>n3:
+        largest = n1
+elif n2>n1 and n2>n3:
+        largest = n2
+else: 
+        largest = n3
+
+print("Largest number among {} {} and {} is {}". format(n1,n2,n3,largest))
+
